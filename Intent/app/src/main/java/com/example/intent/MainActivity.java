5d1bac9418b9dcdab7801b2d27da049bbc221f79
Febriansyah Agung Tirta <febriansyahagungtirta@gmail.com>
2023-10-05 09:54:39 +0700

    Belajar Implicit Intent
    Pertemuan 4 kuliah Pemrograman berbasis mobile

--- Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.intent;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvResult;
+    final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == MoveForResultActivity.RESULT_CODE && result.getData() !=null) {
+                    int selectedValue = result.getData().getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                    tvResult.setText(String.format("Hasil: %s", selectedValue));
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+
+        Button btnMoveForResult = findViewById(R.id.btn_move_for_result);
+        btnMoveForResult.setOnClickListener(this);
+
+        tvResult = findViewById(R.id.tv_result);
     }
 
     @Override
@@ -28,6 +49,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_KULIAH, "PEMROGRAMAN BERBASIS MOBILE");
             moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_KELAS, 5);
             startActivity(moveWithDataIntent);
+        } else if (view.getId() == R.id.btn_dial_number) {
+            String phoneNumber = "085809234920";
+            Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+            startActivity(dialPhoneIntent);
+        } else if (view.getId() == R.id.btn_move_for_result) {
+            Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+            resultLauncher.launch(moveForResultIntent);
         }
     }
 }
